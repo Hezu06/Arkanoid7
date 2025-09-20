cd184da4f46d088e55cac1d00113610922017f24
Hezu <nguyentrunghieu14112006@gmail.com>
2025-09-20 21:57:09 +0700

    create abstract class MovableObject

---
+++ src/main/java/com/arkanoid/entity/MovableObject.java
@@ -0,0 +1,23 @@
+package main.java.com.arkanoid.entity;
+
+import java.awt.*;
+
+public abstract class MovableObject extends GameObject {
+    protected double dx;
+    protected double dy;
+
+    public MovableObject(double x, double y, int width, int height, double dx, double dy) {
+        super(x, y, width, height);
+        this.dy = dy;
+    }
+
+    public void update() {
+
+    }
+
+    public void render(Graphics g) {
+
+    }
+
+    public abstract void move();
+}
