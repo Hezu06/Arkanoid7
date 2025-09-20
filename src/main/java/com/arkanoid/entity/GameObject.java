0d78e34e6b360088a47db8e2344b65fcec7f7a60
Hezu <nguyentrunghieu14112006@gmail.com>
2025-09-20 21:49:26 +0700

    create abstract class GameObject

---
+++ src/main/java/com/arkanoid/entity/GameObject.java
@@ -0,0 +1,50 @@
+package main.java.com.arkanoid.entity;
+
+import java.awt.*;
+
+public abstract class GameObject {
+    protected double x, y;
+    protected int width, height;
+
+    public GameObject(double x, double y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public abstract void update();
+    public abstract void render(Graphics g);
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
