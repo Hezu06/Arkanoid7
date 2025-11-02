c7b32e1ebe9158640af3e20466fcede8f215ebcd
Hezu <nguyentrunghieu14112006@gmail.com>
2025-11-02 21:45:58 +0700

    add(fireBall): create fireBall powerUp

--- src/main/java/com/arkanoid/entity/powerUp/FireBall.java
+++ src/main/java/com/arkanoid/entity/powerUp/FireBall.java
@@ -1,1 +1,26 @@
 package com.arkanoid.entity.powerUp;
+
+import com.arkanoid.entity.Ball;
+import com.arkanoid.game.GameMain;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.util.Objects;
+
+class FireBall extends PowerUp {
+    public FireBall(double x, double y) {
+        super(x, y, 32, 32);
+        super.setImage(new Image(Objects.requireNonNull(getClass().getClassLoader().
+                getResourceAsStream("powerUpItem/green.png"))));
+    }
+    @Override
+    public void applyEffect(GameMain game) {
+        Image ballImg = new Image(Objects.requireNonNull(getClass().
+                getResourceAsStream("/assets/Ball/billiard.png")));
+        ImageView ballImageView = new ImageView(ballImg);
+        for (Ball ball : game.getListBalls()) {
+            Ball.setImage(ballImageView.getImage());
+            ball.activateFireBall();
+        }
+    }
+}
