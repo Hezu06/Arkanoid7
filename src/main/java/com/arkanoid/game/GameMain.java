deb62c32a4961ec7d5e14a46d8b547b6eb13abb2
LeVietAn <chunghieu206@gmail.com>
2025-11-02 15:21:26 +0700

    update ScoreScreen and Linked to mainMenu and playAgain

--- src/main/java/com/arkanoid/game/GameMain.java
+++ src/main/java/com/arkanoid/game/GameMain.java
@@ -8,6 +8,7 @@ import com.arkanoid.level.Level;
 import com.arkanoid.level.LevelLoader;
 import com.arkanoid.ui.ButtonEffects;
 import com.arkanoid.ui.GameButton;
+import com.arkanoid.ui.ScoreScreen;
 import javafx.application.Application;
 import javafx.animation.AnimationTimer;
 import javafx.geometry.Pos;
@@ -52,6 +53,41 @@ public class GameMain extends Application {
     private boolean playAgainShown = false;
     private boolean paused = false;
     private Pane gamePane;
+    private Stage primaryStage;
+
+    // --- Constructor GameMain  ---
+    public GameMain() {}
+    public GameMain(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
+
+    // --- Getter gamePane  ---
+    public Pane getGamePane() {
+        return gamePane;
+    }
+
+    // --- Getter/Setter Paused  ---
+    public boolean isPaused() {
+        return paused;
+    }
+    public void setPaused(boolean paused) {
+        this.paused = paused;
+    }
+
+    // --- Getter backgroundTexture  ---
+    public ImageView getBackgroundTexture() {
+        return backgroundTexture;
+    }
+
+    // --- Getter ballTexture  ---
+    public ImageView getBallTexture() {
+        return ballTexture;
+    }
+
+    // --- Getter paddleTexture  ---
+    public ImageView getPaddleTexture() {
+        return paddleTexture;
+    }
 
     // --- Launch State ---
     private boolean isBallReadyToLaunch = true;
@@ -247,7 +283,7 @@ public class GameMain extends Application {
                     else {
                         // Game Over, show Play Again.
                         if (!playAgainShown) {
-                            showPlayAgainButton();
+                            showPlayAgain();
                             playAgainShown = true;
                         }
                     }
@@ -350,7 +386,7 @@ public class GameMain extends Application {
         activeExplosion.removeIf(ExplosionEffect::isFinished);
 
         if (listBalls.isEmpty() && Ball.getNumberOfBalls() <= 0 && !playAgainShown) {
-            showPlayAgainButton(); // Hàm này sẽ set paused = true
+            showPlayAgain(); // Hàm này sẽ set paused = true
             playAgainShown = true; // Đặt cờ này ở đây
         }
     }
@@ -382,7 +418,7 @@ public class GameMain extends Application {
         }
     }
 
-    private void resetGame() {
+    public void resetGame() {
         System.out.println("Resetting Game");
         playAgainShown = false;
 
@@ -418,27 +454,29 @@ public class GameMain extends Application {
         gamePane.getChildren().addAll(backgroundTexture, canvas);
     }
 
-    private void showPlayAgainButton() {
+    private void showPlayAgain() {
         paused = true;
+        ScoreScreen scoreScreen = new ScoreScreen(primaryStage, 10, this);
 
-        String gameOverMessage = "PLAY AGAIN";
-        GameButton playAgainBtn = new GameButton(gameOverMessage);
-
-        playAgainBtn.setFont(Font.loadFont(
-                getClass().getResourceAsStream("/fonts/ALIEN5.TTF"), 36
-        ));
-        ButtonEffects.applyHoverEffect(playAgainBtn);
-        VBox box = new VBox(playAgainBtn);
-        box.setAlignment(Pos.CENTER);
-        box.setPrefSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-
-        gamePane.getChildren().add(box);
-
-        playAgainBtn.setOnAction(e -> {
-            gamePane.getChildren().clear(); // Delete everything.
-            paused = false;
-            resetGame(); // Re-initialize.
-        });
+        scoreScreen.show();
+//        'String gameOverMessage = "PLAY AGAIN";
+//        GameButton playAgainBtn = new GameButton(gameOverMessage);
+//
+//        playAgainBtn.setFont(Font.loadFont(
+//                getClass().getResourceAsStream("/fonts/ALIEN5.TTF"), 36
+//        ));
+//        ButtonEffects.applyHoverEffect(playAgainBtn);
+//        VBox box = new VBox(playAgainBtn);
+//        box.setAlignment(Pos.CENTER);
+//        box.setPrefSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+//
+//        gamePane.getChildren().add(box);
+//
+//        playAgainBtn.setOnAction(e -> {
+//            gamePane.getChildren().clear(); // Delete everything.
+//            paused = false;
+//            resetGame(); // Re-initialize.
+//        });'
     }
 
     public GameStateManager getGameStateManager() {
--- src/main/java/com/arkanoid/ui/ScoreScreen.java
+++ src/main/java/com/arkanoid/ui/ScoreScreen.java
@@ -1,19 +1,32 @@
 package com.arkanoid.ui;
 
+import com.arkanoid.game.GameMain;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
-public class ScoreScreen {
+public class ScoreScreen  {
+    static final int WIDTH = 750;
+    static final int HEIGHT = 800;
     private int score;
     private Stage stage;
+    private GameMain gameMain;
+    private Stage primaryStage;
 
-    public ScoreScreen(Stage stage, int score) {
-        this.stage = stage;
+    public ScoreScreen() {
+
+    }
+
+    public ScoreScreen(Stage primaryStage, int score, GameMain gameMain) {
+        this.primaryStage = primaryStage;
         this.score = score;
+        this.gameMain = gameMain;
     }
 
     public void show() {
@@ -23,33 +36,54 @@ public class ScoreScreen {
         Label scoreLabel = new Label("Your score: " + score);
         scoreLabel.setStyle("-fx-font-size: 24px; -fx-text-fill: #222;");
 
-        GameButton retryBtn = new GameButton("Play Again");
-        GameButton mainMenuBtn = new GameButton("Main Menu");
-        GameButton exitBtn = new GameButton("Exit");
+        GameButton retryBtn = new GameButton("PLAY AGAIN");
+        GameButton mainMenuBtn = new GameButton("MAIN MENU");
+        GameButton exitBtn = new GameButton("EXIT");
 
         for (Button b : new Button[]{retryBtn, mainMenuBtn, exitBtn}) {
             ButtonEffects.applyHoverEffect(b);
         }
 
+        VBox root = new VBox(20, title, scoreLabel, retryBtn, mainMenuBtn, exitBtn);
+        root.setAlignment(Pos.CENTER);
+        root.setPrefSize(WIDTH, HEIGHT);
+
+        Pane overPane = new Pane(root);
+        gameMain.getGamePane().getChildren().add(overPane);
+        // Set Font
+        retryBtn.setFont(Font.loadFont(
+                getClass().getResourceAsStream("/fonts/ALIEN5.TTF"), 36
+        ));
+
+        mainMenuBtn.setFont(Font.loadFont(
+                getClass().getResourceAsStream("/fonts/ALIEN5.TTF"), 36
+        ));
+
+        exitBtn.setFont(Font.loadFont(
+                getClass().getResourceAsStream("/fonts/ALIEN5.TTF"), 36
+        ));
         // Nút chơi lại
         retryBtn.setOnAction(e -> {
-            
+            gameMain.getGamePane().getChildren().clear(); // Xóa toàn bộ
+            gameMain.setPaused(false);
+            gameMain.resetGame(); // Khởi tạo lại
         });
 
         // Nút quay về menu chính
         mainMenuBtn.setOnAction(e -> {
-
+            try {
+                GameMenu gameMenu = new GameMenu(gameMain.getBackgroundTexture(),
+                                                    gameMain.getBallTexture(), gameMain.getPaddleTexture());
+                gameMain.getGamePane().getChildren().clear();
+                gameMain.setPaused(false);
+                gameMenu.start(primaryStage);
+            }
+            catch (Exception ex) {
+                ex.printStackTrace();
+            }
         });
 
         // Nút thoát
-
-        exitBtn.setOnAction(e -> stage.close());
-
-        VBox root = new VBox(20, title, scoreLabel, retryBtn, mainMenuBtn, exitBtn);
-        root.setAlignment(Pos.CENTER);
-        root.setStyle("-fx-background-color: linear-gradient(to bottom, #ffe, #ccc);");
-
-        Scene scene = new Scene(root, 750, 950);
-        stage.setScene(scene);
+        exitBtn.setOnAction(e -> primaryStage.close());
     }
 }
