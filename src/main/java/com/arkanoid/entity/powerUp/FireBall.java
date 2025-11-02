0516aa068b63faa7263f1b389725c133dda48df6
Hezu <nguyentrunghieu14112006@gmail.com>
2025-11-02 22:08:34 +0700

    update fireBall image

--- src/main/java/com/arkanoid/entity/powerUp/FireBall.java
+++ src/main/java/com/arkanoid/entity/powerUp/FireBall.java
@@ -11,12 +11,12 @@ class FireBall extends PowerUp {
     public FireBall(double x, double y) {
         super(x, y, 32, 32);
         super.setImage(new Image(Objects.requireNonNull(getClass().getClassLoader().
-                getResourceAsStream("powerUpItem/green.png"))));
+                getResourceAsStream("powerUpItem/fire.png"))));
     }
     @Override
     public void applyEffect(GameMain game) {
         Image ballImg = new Image(Objects.requireNonNull(getClass().
-                getResourceAsStream("/assets/Ball/billiard.png")));
+                getResourceAsStream("/assets/Ball/fireBall.png")));
         ImageView ballImageView = new ImageView(ballImg);
         for (Ball ball : game.getListBalls()) {
             Ball.setImage(ballImageView.getImage());
