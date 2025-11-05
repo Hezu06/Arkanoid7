704e83f045936ff165f1c2c9c5657e6564aee486
Hezu <nguyentrunghieu14112006@gmail.com>
2025-11-05 22:48:28 +0700

    fix timeline of laser, update laser effect by deltaTime

--- src/main/java/com/arkanoid/entity/powerUp/Laser.java
+++ src/main/java/com/arkanoid/entity/powerUp/Laser.java
@@ -2,18 +2,15 @@ package com.arkanoid.entity.powerUp;
 
 import com.arkanoid.entity.Paddle;
 import com.arkanoid.game.GameMain;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
-import javafx.util.Duration;
+// Xóa import Timeline và KeyFrame
 
 import java.util.Objects;
 
 public class Laser extends PowerUp {
-    private static final int DURATION_MS = 5000; // 5 giây
-    private static final int SHOOT_INTERVAL_MS = 250;
-    private Timeline laserTimeline;
+    // Xóa các hằng số DURATION_MS, SHOOT_INTERVAL_MS
+    // Xóa trường laserTimeline
 
     public Laser(double x, double y) {
         super(x, y, 32, 32);
@@ -30,49 +27,18 @@ public class Laser extends PowerUp {
     public void applyEffect(GameMain game) {
         Paddle paddle = game.getPaddle();
 
-        paddle.setLaserPowerUpInEffect(true);
-        paddle.setLaserInterrupted(false);
+        // Chỉ cần gọi một phương thức trên paddle
+        // Chúng ta truyền 'game' vào để paddle biết nơi spawn tia laser
+        paddle.activateLaserPowerUp(game);
+
         System.out.println("Laser PowerUp activated!");
 
-        laserTimeline = new Timeline(new KeyFrame(Duration.millis(SHOOT_INTERVAL_MS), e -> {
-            // Nếu game bị reset hoặc paddle không còn tồn tại thì dừng
-            if (!paddle.isLaserPowerUpInEffect()) {
-                stopLaser(paddle);
-                return;
-            }
+        // Xóa tất cả code Timeline...
 
-            // Tạo 2 tia laser bắn ra từ hai bên paddle
-            double leftX = paddle.getX() + 10;
-            double rightX = paddle.getX() + paddle.getWidth() - 14;
-            double y = paddle.getY() - 15;
-
-            game.getLaserBeams().add(new LaserBeam(leftX, y));
-            game.getLaserBeams().add(new LaserBeam(rightX, y));
-        }));
-
-        // Số lần bắn = tổng thời gian / thời gian mỗi lần
-        int repeatCount = DURATION_MS / SHOOT_INTERVAL_MS;
-        laserTimeline.setCycleCount(repeatCount);
-
-        // Khi timeline kết thúc thì tắt laser
-        laserTimeline.setOnFinished(e -> stopLaser(paddle));
-
-        laserTimeline.play();
+        // Tự hủy (viên power-up biến mất sau khi nhặt)
         deactivate();
     }
 
-    private void stopLaser(Paddle paddle) {
-        if (laserTimeline != null) {
-            laserTimeline.stop();
-        }
-        paddle.setLaserPowerUpInEffect(false);
-        System.out.println("Laser PowerUp ended!");
-    }
-
-    public void stopImmediately() {
-        // Hàm này có thể gọi khi reset game
-        if (laserTimeline != null) {
-            laserTimeline.stop();
-        }
-    }
+    // Xóa phương thức stopLaser()
+    // Xóa phương thức stopImmediately()
 }
